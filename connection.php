bc92183fe4ef3d7c493bc47a13070bae66d8482d
dkrkv1107 <dkrkv1107@icloud.com>
2022-05-13 21:11:54 +0530

    Completed signup

--- connection.php
+++ connection.php
@@ -1,20 +1,15 @@
 <?php 
-if($_SERVER['REQUEST_METHOD']== 'POST' && isset($_POST['submit'])){
-    $conn= mysqli_connect('localhost','root','','chat-db') or die("connection failed:" .mysqli_connect_error());
-    if(isset($_POST['name']) && isset($_POST['username']) && isset($_POST['password'])){
-        $name= $_POST['name'];
-        $username= $_POST['username'];
-        $password= $_POST['password'];
-$sql= "INSERT INTO `users` (`name`, 'username', 'password') VALUES(`$name`, '$username', `$password`)";
-    $query = mysqli_query($conn,$sql);
-    if($query){
-        echo "entry succesfull";
-    }
-    else 'connection error';
+$sNAME = "localhost";
+$uNAME = "root";
+$pass = "";
+$db_name = "chat_db";
+
+try{
+    $conn = new PDO("mysql:host=$sNAME;dbname =$db_name,$uNAME,$pass");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}catch(PDOException $e){
+    echo "connection failed :". $e->getMessage();
 }
 
-}
-
-
-
-?>
+?> 
--- signup2.php
+++ signup2.php
@@ -1,8 +1,8 @@
 <?php
 
 if(isset($_POST['username'])&&
-isset($_POST['username'])&&
-isset($_POST['username'])){
+isset($_POST['name'])&&
+isset($_POST['password'])){
 
 include "connection.php";
 
@@ -28,7 +28,59 @@ include "connection.php";
         exit;
     }
     else{
-        echo "good";
+       $sql = "SELECT username FROM users WHERE username=?";
+       $stmt = $conn->prepare($sql);
+       $stmt->execute([$username]);
+
+       if($stmt->rowCount() > 0){
+           $em = "The username ($username) is taken";
+           header("Location: signup.php?error=$em&$data");
+           exit;
+
+       }
+       else{
+           if(isset($_FILES['profilepic'])){
+            $img_name = $_FILES['profilepic'];
+            $tmp_name = $_FILES['profilepic']['tmp_name'];
+            $error = $_FILES['profilepic']['error'];
+
+            if($error === 0){
+$img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+$img_ex_lc = strtolower($img_ex);
+$allowed_exs = array("jpg","jpeg","png") ;
+
+if(in_array($img_ex_lc, $allowed_exs)){
+$new_img_name = $username. '.' .$img_ex_lc;
+$img_upload_path = '../../uploads/'.$new_img_name;
+move_uploaded_file($tmp_name,$img_upload_path);
+}       else{
+           $em = "Change image file type";
+           header("Location: signup.php?error=$em&$data");
+           exit; 
+                }
+            }
+            else{
+                $em = "Unknown error occured";
+           header("Location: signup.php?error=$em&$data");
+           exit; 
+            }
+
+           }
+           $password = password_hash($password, PASSWORD_DEFAULT);
+           if(isset($new_img_name)){
+               $sql = "INSERT INTO users (name,username,password,p_p)VALUES(?,?,?,?) ";
+               $stmt = $conn->prepare($sql);
+               $stmt->execute([$name,$username,$password,$new_img_name]);
+           }
+           else{
+            $sql = "INSERT INTO users (name,username,password,)VALUES(?,?,?) ";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$name,$username,$password,]);
+           }
+           $sm = "Account created succesfully";
+           header("Location: /index.php?seccess=$sm");
+           exit;
+       }
     }
 }
 
