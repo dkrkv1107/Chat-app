165a6de609caa646d98575286c64973fd0abcebc
dkrkv1107 <dkrkv1107@icloud.com>
2022-05-16 19:54:04 +0530

    added more backend files

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,33 +9,32 @@
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/d4a213e92e.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-  <div class="wrapper">
-    <section class="form login">
-      <header>Realtime Chat App</header>
-      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
-        <div class="error-text">This is an error message</div>
-        
-        <div class="field input">
-          <label>Email Address</label>
-          <input type="text" name="email" placeholder="Enter your email" required>
-        </div>
-        <div class="field input">
-          <label>Password</label>
-          <input type="password" name="password" placeholder="Enter new password" required>
-          <i class="fas fa-eye"></i>
-        </div>
-       
-        <div class="field button">
-          <input type="submit" name="submit" value="Continue to Chat">
-        </div>
-      </form>
-      <div class="link">Not yet signed up? <a href="login.php">Sign-up now</a></div>
-    </section>
-  </div>
+    <div class="wrapper">
+        <section class="form login">
+            <header>Realtime Chat App</header>
+            <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+                <div class="error-text">This is an error message</div>
 
-  <script src="javascript/pass-show-hide.js"></script>
-  
+                <div class="field input">
+                    <label>Email Address</label>
+                    <input type="text" name="email" placeholder="Enter your email" required>
+                </div>
+                <div class="field input">
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="Enter new password" required>
+                    <i class="fas fa-eye"></i>
+                </div>
 
+                <div class="field button">
+                    <input type="submit" name="submit" value="Continue to Chat">
+                </div>
+            </form>
+            <div class="link">Not yet signed up? <a href="login.php">Sign-up now</a></div>
+        </section>
+    </div>
+
+    <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/login.js"></script>
 </body>
-</html>
