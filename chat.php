8e6d7316553b52c6a16227fad90af0483f58d887
dkrkv1107 <dkrkv1107@icloud.com>
2022-05-17 21:39:41 +0530

    changed files

--- chat.php
+++ chat.php
@@ -1,50 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat-app</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/d4a213e92e.js" crossorigin="anonymous"></script>
-</head>
+<?php
+include_once "header.php";
+
+?>
+
 <body>
-  <div class="wrapper">
-    <section class="chat-area">
-     <header>
-         <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-         <div class="content" style="display: flex;">
-             <img src="img.png" alt="">
-             <div class="details">
-                 <span>User Name</span>
-                 <p>Active Now</p>
+    <div class="wrapper">
+        <section class="chat-area">
+            <header>
+                <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <div class="content" style="display: flex;">
+                    <img src="img.png" alt="">
+                    <div class="details">
+                        <span>User Name</span>
+                        <p>Active Now</p>
 
-             </div>
-         </div>
-       
-     </header>
-     <div class="chat-box">
-         <div class="chat outgoing">
-             <div class="details">
-                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa provident voluptates</p>
-             </div>
-            
-         </div>
-         <div class="chat incoming">
-             <img src="img.png" alt=" ">
-             <div class="details">
-             <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa provident voluptates</p>
-             </div>
-          </div>
-     </div>
-     <form action="#" class="typing-area">
-         <input type="text" placeholder="Type message to send.....">
-         <button><i class="fab fa-telegram-plane"></i></button>
-     </form>
-    </section>
-  </div>
+                    </div>
+                </div>
 
-  
+            </header>
+            <div class="chat-box">
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa provident voluptates</p>
+                    </div>
+
+                </div>
+                <div class="chat incoming">
+                    <img src="img.png" alt=" ">
+                    <div class="details">
+                        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa provident voluptates</p>
+                    </div>
+                </div>
+            </div>
+            <form action="#" class="typing-area">
+                <input type="text" placeholder="Type message to send.....">
+                <button><i class="fab fa-telegram-plane"></i></button>
+            </form>
+        </section>
+    </div>
+
+
 
 </body>
+
 </html>
--- users.php
+++ users.php
@@ -1,54 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat-app</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/d4a213e92e.js" crossorigin="anonymous"></script>
-</head>
+<?php
+include_once "header.php";
+?>
+
 <body>
-  <div class="wrapper">
-    <section class="users">
-     <header>
-         <div class="content">
-             <img src="img.png" alt="">
-             <div class="details">
-                 <span>User Name</span>
-                 <p>Active Now</p>
+    <div class="wrapper">
+        <section class="users">
+            <header>
+                <div class="content">
+                    <img src="img.png" alt="">
+                    <div class="details">
+                        <span>User Name</span>
+                        <p>Active Now</p>
 
-             </div>
-         </div>
-         <a href="#" class="logout">
-              Logout
-         </a>
-     </header>
-     <div class="search">
-         <span class="text">
-             Select User To Start Chat
-         </span>
-         <input type="text" placeholder="Enter Name To Search....">
-         <button><i class="fas fa-search"></i></button>
-     </div>
-     <div class="users-list">
-         <a href="#">
-         <div class="content">
-             <img src="img.png" alt="">
-             <div class="details">
-                 <span>User Name</span>
-                 <p>Test Message</p>
+                    </div>
+                </div>
+                <a href="#" class="logout">
+                    Logout
+                </a>
+            </header>
+            <div class="search">
+                <span class="text">
+                    Select User To Start Chat
+                </span>
+                <input type="text" placeholder="Enter Name To Search....">
+                <button><i class="fas fa-search"></i></button>
+            </div>
+            <div class="users-list">
+                <a href="#">
+                    <div class="content">
+                        <img src="img.png" alt="">
+                        <div class="details">
+                            <span>User Name</span>
+                            <p>Test Message</p>
 
-             </div>
-         </div>
-         <div class="status-dot"><i class="fas fa-circle"></i></div>
-         </a>
-     </div>
-    </section>
-  </div>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>
+            </div>
+        </section>
+    </div>
 
-  <script src="javascript/users.js"></script>
-  
+    <script src="javascript/users.js"></script>
+
 
 </body>
+
 </html>
